aa917c9adf268ba298daed236cec0600059a0e6f
tmcguiggan9 <33290852+tmcguiggan9@users.noreply.github.com>
2023-09-12 16:37:56 -0400

    revert authenticator
    revert authenticator

--- DocGPT/Authentication/Authenticator.swift
+++ DocGPT/Authentication/Authenticator.swift
@@ -10,119 +10,224 @@ import MSAL
 import Foundation
 import Security
 
-struct Constants {
-    static let tenantName = "docgptb2c.onmicrosoft.com"
-    static let authorityHostName = "docgptb2c.b2clogin.com"
-    static let clientID = "3493c6b7-3120-44c9-9650-fc5a60525146"
-    static let signupOrSigninPolicy = "B2C_1_signupsignin"
-    static let endpoint = "https://%@/tfp/%@/%@"
-    static let scopes = ["https://docgptb2c.onmicrosoft.com/test/store_data"]
-}
-
 class Authenticator: ObservableObject {
+
     @Published var accessToken: String?
     @Published var userID: String?
+    let kTenantName = "docgptb2c.onmicrosoft.com" // Your tenant name
+    let kAuthorityHostName = "docgptb2c.b2clogin.com" // Your authority host name
+    let kClientID = "3493c6b7-3120-44c9-9650-fc5a60525146" // Your client ID from the portal when you created your application
+    let kSignupOrSigninPolicy = "B2C_1_signupsignin" // Your signup and sign-in policy you created in the portal
+    let kEditProfilePolicy = "b2c_1_edit_profile" // Your edit policy you created in the portal
+    let kResetPasswordPolicy = "b2c_1_reset" // Your reset password policy you created in the portal
+    let kGraphURI = "https://doc-gpt-app.azurewebsites.net/your-route" // This is your backend API that you've configured to accept your app's tokens
+    let kScopes: [String] = ["https://docgptb2c.onmicrosoft.com/test/store_data"] // This is a scope that you've configured your backend API to look for.
     
-    private var application: MSALPublicClientApplication!
-    private var view: UIViewController
+    // DO NOT CHANGE - This is the format of OIDC Token and Authorization endpoints for Azure AD B2C.
+    let kEndpoint = "https://%@/tfp/%@/%@"
+
+    var application: MSALPublicClientApplication!
+
+    var view: UIViewController
+
 
     init(view: UIViewController) {
         self.view = view
     }
 
-    private func createPCAConfig(forPolicy policy: String) throws -> MSALPublicClientApplication {
-        let authority = try getAuthority(forPolicy: policy)
-        let pcaConfig = MSALPublicClientApplicationConfig(clientId: Constants.clientID, redirectUri: "msauth.com.tmcguiggan.DocGPT://auth", authority: authority)
-        pcaConfig.knownAuthorities = [authority]
-        return try MSALPublicClientApplication(configuration: pcaConfig)
-    }
-
     func baseAuth(completionHandler: @escaping (String?, String?, String?) -> Void) {
         do {
-            self.application = try createPCAConfig(forPolicy: Constants.signupOrSigninPolicy)
-            refreshToken(completionHandler: completionHandler)
-        } catch {
-            print("Unable to create application: \(error)")
-        }
-    }
+            let siginPolicyAuthority = try self.getAuthority(forPolicy: self.kSignupOrSigninPolicy)
 
-    private func handleResult(_ result: MSALResult?, _ error: Error?, completionHandler: @escaping (String?, String?, String?) -> Void) {
-        guard let result = result else {
-            print("Could not acquire token: \(error ?? "No error information" as Error)")
-            return
+            let pcaConfig = MSALPublicClientApplicationConfig(clientId: kClientID, redirectUri: "msauth.com.tmcguiggan.DocGPT://auth", authority: siginPolicyAuthority)
+            pcaConfig.knownAuthorities = [siginPolicyAuthority]
+            self.application = try MSALPublicClientApplication(configuration: pcaConfig)
+        } catch {
+            print("Unable to create application \(error)")
         }
-        
-        accessToken = result.accessToken
-        if let accountId = result.account.homeAccountId?.identifier,
-           let idToken = result.idToken,
-           let claims = parseIdToken(idToken),
-           let givenName = claims["given_name"] as? String {
-            userID = accountId
-            completionHandler(accessToken, accountId, givenName)
+        refreshToken { token, userID, givenName  in
+            completionHandler(token, userID, givenName)
         }
     }
 
     func authenticateUser(completionHandler: @escaping (String?, String?, String?) -> Void) {
         do {
-            let authority = try getAuthority(forPolicy: Constants.signupOrSigninPolicy)
+            let authority = try self.getAuthority(forPolicy: self.kSignupOrSigninPolicy)
             let webViewParameters = MSALWebviewParameters(authPresentationViewController: view)
-            let parameters = MSALInteractiveTokenParameters(scopes: Constants.scopes, webviewParameters: webViewParameters)
+            let parameters = MSALInteractiveTokenParameters(scopes: kScopes, webviewParameters: webViewParameters)
             parameters.promptType = .selectAccount
             parameters.authority = authority
             application.acquireToken(with: parameters) { (result, error) in
-                self.handleResult(result, error, completionHandler: completionHandler)
+
+                guard let result = result else {
+                    print("Could not acquire token: \(error ?? "No error informarion" as Error)")
+                    return
+                }
+
+                self.accessToken = result.accessToken
+                let accessToken = result.accessToken
+                print("Access token is \(accessToken )")
+
+                if let accountId = result.account.homeAccountId?.identifier, let idToken = result.idToken,
+                   let claims = self.parseIdToken(idToken),
+                       let givenName = claims["given_name"] as? String {
+                    print("User ID is \(accountId)")
+                    completionHandler(accessToken, accountId, givenName)
+                }
+
+
+                if let idToken = result.idToken,
+                   let claims = self.parseIdToken(idToken),
+                       let givenName = claims["given_name"] as? String {
+                        print("GivenName is \(givenName)")
+                    }
             }
         } catch {
-            print("Unable to create authority: \(error)")
+            print("Unable to create authority \(error)")
         }
     }
+
 
     func parseIdToken(_ idToken: String) -> [String: Any]? {
         let parts = idToken.split(separator: ".")
-        guard parts.count == 3, let jwtPayloadData = Data(base64Encoded: String(parts[1]), options: .ignoreUnknownCharacters) else { return nil }
-        
-        return (try? JSONSerialization.jsonObject(with: jwtPayloadData, options: [])) as? [String: Any]
-    }
+        guard parts.count == 3 else { return nil }
 
-    func refreshToken(completionHandler: @escaping (String?, String?, String?) -> Void) {
+        let jwtPayloadData = Data(base64Encoded: String(parts[1]), options: .ignoreUnknownCharacters)
+        guard let data = jwtPayloadData else { return nil }
+
         do {
-            let authority = try getAuthority(forPolicy: Constants.signupOrSigninPolicy)
-            guard let thisAccount = try getAccountByPolicy(withAccounts: application.allAccounts(), policy: Constants.signupOrSigninPolicy) else {
-                authenticateUser(completionHandler: completionHandler)
-                return
-            }
-            
-            let parameters = MSALSilentTokenParameters(scopes: Constants.scopes, account: thisAccount)
-            parameters.authority = authority
-            application.acquireTokenSilent(with: parameters) { (result, error) in
-                self.handleResult(result, error, completionHandler: completionHandler)
-            }
+            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            return json as? [String: Any]
         } catch {
-            print("Error: \(error)")
+            print("Error parsing idToken: \(error)")
+            return nil
         }
     }
+
+
+    func refreshToken(completionHandler: @escaping (String?, String?, String?) -> Void) {
+
+        do {
+            let authority = try self.getAuthority(forPolicy: self.kSignupOrSigninPolicy)
+
+            guard let thisAccount = try self.getAccountByPolicy(withAccounts: application.allAccounts(), policy: kSignupOrSigninPolicy) else {
+                authenticateUser { token, userID, givenName  in
+                    completionHandler(token, userID, givenName)
+                }
+                return
+            }
+
+            let parameters = MSALSilentTokenParameters(scopes: kScopes, account:thisAccount)
+            parameters.authority = authority
+            self.application.acquireTokenSilent(with: parameters) { (result, error) in
+                if let error = error {
+
+                    let nsError = error as NSError
+
+                    if (nsError.domain == MSALErrorDomain) {
+
+                        if (nsError.code == MSALError.interactionRequired.rawValue) {
+                            let webviewParameters = MSALWebviewParameters(authPresentationViewController: self.view)
+                            let parameters = MSALInteractiveTokenParameters(scopes: self.kScopes, webviewParameters: webviewParameters)
+                            parameters.account = thisAccount
+
+                            DispatchQueue.main.async {
+                                self.application.acquireToken(with: parameters) { (result, error) in
+
+                                    guard let result = result else {
+                                        print("Could not acquire new token: \(error ?? "No error informarion" as Error)")
+                                        return
+                                    }
+
+
+                                    if let accountID = result.account.homeAccountId?.identifier,
+                                    let idToken = result.idToken,
+                                    let claims = self.parseIdToken(idToken),
+                                       let givenName = claims["given_name"] as? String {
+                                        completionHandler(result.accessToken, accountID, givenName)
+                                    }
+
+                                }
+                            }
+                            return
+                        }
+                    }
+
+                    print("Could not acquire token: \(error)")
+                    return
+                }
+                guard let result = result else {
+
+                    print("Could not acquire token: No result returned")
+                    return
+                }
+
+                self.accessToken = result.accessToken
+                let accessToken = result.accessToken
+
+                if let accountId = result.account.homeAccountId?.identifier, let idToken = result.idToken,
+                   let claims = self.parseIdToken(idToken),
+                       let givenName = claims["given_name"] as? String {
+                    self.userID = accountId
+                    completionHandler(accessToken, accountId, givenName)
+                }
+
+                if let idToken = result.idToken,
+                   let claims = self.parseIdToken(idToken),
+                       let givenName = claims["given_name"] as? String {
+                        print("GivenName is \(givenName)")
+                    }
+
+                print("Refreshing token silently")
+            }
+        } catch {
+            print("Unable to construct parameters before calling acquire token \(error)")
+        }
+    }
+
+
 
     func signoutUser() {
         do {
-            let thisAccount = try getAccountByPolicy(withAccounts: application.allAccounts(), policy: Constants.signupOrSigninPolicy)
+
+            let thisAccount = try self.getAccountByPolicy(withAccounts: application.allAccounts(), policy: kSignupOrSigninPolicy)
+
             if let accountToRemove = thisAccount {
                 try application.remove(accountToRemove)
-                print("Signed out")
             } else {
-                print("No account to sign out!")
+                print("There is no account to signing out!")
             }
-        } catch {
-            print("Error signing out: \(error)")
+
+
+
+            print("Signed out")
+
+        } catch  {
+            print("Received error signing out: \(error)")
         }
     }
 
-    private func getAccountByPolicy(withAccounts accounts: [MSALAccount], policy: String) -> MSALAccount? {
-        return accounts.first(where: { $0.homeAccountId?.objectId?.hasSuffix(policy.lowercased()) == true })
+
+    func getAccountByPolicy (withAccounts accounts: [MSALAccount], policy: String) throws -> MSALAccount? {
+
+        for account in accounts {
+            // This is a single account sample, so we only check the suffic part of the object id,
+            // where object id is in the form of <object id>-<policy>.
+            // For multi-account apps, the whole object id needs to be checked.
+            if let homeAccountId = account.homeAccountId, let objectId = homeAccountId.objectId {
+                if objectId.hasSuffix(policy.lowercased()) {
+                    return account
+                }
+            }
+        }
+        return nil
     }
 
-    private func getAuthority(forPolicy policy: String) throws -> MSALB2CAuthority {
-        guard let authorityURL = URL(string: String(format: Constants.endpoint, Constants.authorityHostName, Constants.tenantName, policy)) else {
-            throw NSError(domain: "SomeDomain", code: 1, userInfo: ["errorDescription": "Unable to create authority URL!"])
+    func getAuthority(forPolicy policy: String) throws -> MSALB2CAuthority {
+        guard let authorityURL = URL(string: String(format: self.kEndpoint, self.kAuthorityHostName, self.kTenantName, policy)) else {
+            throw NSError(domain: "SomeDomain",
+                          code: 1,
+                          userInfo: ["errorDescription": "Unable to create authority URL!"])
         }
         return try MSALB2CAuthority(url: authorityURL)
     }
